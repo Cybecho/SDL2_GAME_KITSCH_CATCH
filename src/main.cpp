411a18d1da3927b399675bc4ff91ceaa2e66a68c
DARAMJll <166911614+DARAMJll@users.noreply.github.com>
2024-05-06 18:50:59 +0900

    main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+//메인함수
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <vector>
+#include <atlstr.h>
+
+#include "SDL.h" 
+#include "SDL_image.h"
+#include "SDL_mixer.h"
+#include "SDL_ttf.h"
+
+#include "MainScreen.h"
