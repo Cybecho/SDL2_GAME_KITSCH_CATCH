981b450cd5877a595aea4bae67a0c3b285d4f1eb
catholicTaeyoung <168614939+catholicTaeyoung@users.noreply.github.com>
2024-05-07 00:08:29 +0900

    Intro, Ending 페이즈 초기 구현

---
+++ src/gameEndingGameover.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "SDL.h"
+#include "gameClass.h"
+
+class gameEndingGameover : public gameClass {
+public:
+	gameEndingGameover();
+	~gameEndingGameover();
+
+	void HandleEvents();
+	void Update();
+	void Render();
+};
+
+extern Mix_Music* intro_music;
+extern Mix_Music* gameover_music;
---
+++ src/gameIntroCartoon.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "SDL.h"
+#include "gameIntro.h"
+
+class Cartoon{
+public:
+	Cartoon();
+	~Cartoon();
+
+	void FadeIn(int seconds);
+	void Delay(int seconds);
+
+	SDL_Surface* surface;
+	SDL_Texture* texture;
+	SDL_Rect source_rect;
+	SDL_Rect destination_rect;
+
+	// FadeIn() 변수
+	int fade_alpha = 0;
+	double fade_value = 0;
+	double fade_delta = 0;
+	bool is_fade_finished = false;
+
+	// Delay() 변수
+	int delay_elapsed_time = 0;
+	bool is_delay_finished = false;
+};
