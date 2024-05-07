779a3564c07ead0abe573a3125330b059a7dec56
DARAMJll <166911614+DARAMJll@users.noreply.github.com>
2024-05-07 19:33:25 +0900

    Update Include.h

--- src/Include.h
+++ src/Include.h
@@ -1,1 +1,13 @@
 // 기본 헤더 파일
+
+#pragma once
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <vector>
+#include <atlstr.h>
+
+#include "SDL.h" 
+#include "SDL_image.h"
+#include "SDL_mixer.h"
+#include "SDL_ttf.h"
