516bd545ec81263643b03782e546172703a86a04
Cybecho_Proxy <42949995+Cybecho@users.noreply.github.com>
2024-05-08 08:15:20 +0900

    Incldue.h 수정

--- src/Include.h
+++ src/Include.h
@@ -1,13 +1,42 @@
-// 기본 헤더 파일
+#pragma once // 헤더가드 대신 pragma once 사용
+#define MOVE_SPEED 20.0f // 이동 속도
+#define WINDOW_WIDTH 540 // 창 너비
+#define WINDOW_HEIGHT 960 // 창 높이
 
-#pragma once
+//! 기본 헤더
 #include <iostream>
 #include <string>
-#include <windows.h>
+#include <queue>
 #include <vector>
+#include <stack>
+#include <cmath>
+#include <ctime>
 #include <atlstr.h>
+#include <windows.h>
 
-#include "SDL.h" 
+//! SDL 헤더
+#include "SDL.h"
 #include "SDL_image.h"
 #include "SDL_mixer.h"
 #include "SDL_ttf.h"
+
+//! 타입 정의
+// using 키워드를 사용하여 타입을 정의 aliasing 하는거임
+using int8 = std::int8_t;
+using int16 = std::int16_t;
+using int32 = std::int32_t;
+using int64 = std::int64_t;
+using uint8 = std::uint8_t;
+using uint16 = std::uint16_t;
+using uint32 = std::uint32_t;
+using uint64 = std::uint64_t;
+
+//! extern 변수
+// extern 이란 변수를 선언만 하고 정의는 다른 곳에서 하는 것
+extern bool g_flag_running;
+extern SDL_Renderer* g_renderer;
+extern Uint32 g_frame_per_sec;
+extern int g_input; // 사용자의 키를 입력받는 변수
+
+//! using namespace
+using namespace std;
