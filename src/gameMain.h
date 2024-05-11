e316c53aeffec1ca7c9edfb904ecbb54f415f83e
Gaeul <166911614+DARAMJll@users.noreply.github.com>
2024-05-11 18:16:10 +0900

    Update gameMain.h

--- src/gameMain.h
+++ src/gameMain.h
@@ -14,7 +14,7 @@ public:
 	void Update();
 	void Render();
 
-	int score;
+	string score;
 	SDL_Texture* main_bg;
 	SDL_Texture* cat;
 	SDL_Texture* inter_bt;
@@ -27,29 +27,9 @@ public:
 	SDL_Rect settingBT_rect;
 
 	//text
-	SDL_Texture* exp_text; //"����ġ" �۾�
-	SDL_Texture* score_text; //����(����ġ)
 	SDL_Rect exp_rect;
 	SDL_Rect score_rect;
+
+	
 };
-/*
-int score;
-
-SDL_Texture* main_bg;
-SDL_Texture* cat;
-SDL_Texture* inter_bt;
-SDL_Texture* play_bt;
-SDL_Texture* setting_bt;
-
-SDL_Rect playBT_rect;
-SDL_Rect cat_rect;
-SDL_Rect interBT_rect;
-SDL_Rect settingBT_rect;
-
-//text
-SDL_Texture* exp_text; //"경험치" 글씨
-SDL_Texture* score_text; //점수(경험치)
-SDL_Rect exp_rect;
-SDL_Rect score_rect;
-
-*/
+extern Mix_Music* main_music;
