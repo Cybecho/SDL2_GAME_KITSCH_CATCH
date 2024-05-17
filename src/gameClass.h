f268523d17bb7003440f3181f0cd694cac15b654
Gaeul <166911614+DARAMJll@users.noreply.github.com>
2024-05-17 22:26:07 +0900

    Timer 클래스 추가

--- src/gameClass.h
+++ src/gameClass.h
@@ -18,6 +18,25 @@ public:
 	SDL_Rect destination_rect;
 };
 
+
+class Timer {
+public:
+	Timer();
+	~Timer();
+
+	void setInterval(int interval); //주기 설정
+	void start();
+	void pause();
+	void resume();
+	bool done(); //시간 다 되었는지 조사
+
+private:
+	double interval;
+	double startTicks;
+	double pauseTicks;
+};
+
+
 void InitGame();
 void ClearGame();
 
@@ -26,3 +45,8 @@ extern bool g_flag_running;
 extern Uint32 g_last_time_ms;
 extern SDL_Renderer* g_renderer;
 extern SDL_Window* g_window;
+
+
+
+
+
