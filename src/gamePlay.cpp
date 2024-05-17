a2e7092e03d082f41d8d7d767f9ca77ff882d4e5
Gaeul <166911614+DARAMJll@users.noreply.github.com>
2024-05-17 22:29:57 +0900

    고양이 애니메이션, 점수 추가

--- src/gamePlay.cpp
+++ src/gamePlay.cpp
@@ -6,15 +6,15 @@ extern SDL_Texture* score_text; //get score from txt file
 gamePlay::gamePlay() {
 
 	// BG
-	SDL_Surface* bg_surface = IMG_Load("../../res/testRes/testPlayBG.png");
+	SDL_Surface* bg_surface = IMG_Load("../../res/game page/game_background.png");
 	play_bg = SDL_CreateTextureFromSurface(g_renderer, bg_surface);
 	SDL_FreeSurface(bg_surface);
 
 
 
-
+	
 	//cat
-	SDL_Surface* cat_surface = IMG_Load("../../res/testRes/testMainCat.png");
+	SDL_Surface* cat_surface = IMG_Load("../../res/main page/main_cat1.png");
 	cat = SDL_CreateTextureFromSurface(g_renderer, cat_surface);
 
 	cat_rect.x = 0;
@@ -25,6 +25,18 @@ gamePlay::gamePlay() {
 	SDL_FreeSurface(cat_surface);
 
 
+
+	SDL_Surface* cat_surface2 = IMG_Load("../../res/main page/main_cat2.png");
+	cat2 = SDL_CreateTextureFromSurface(g_renderer, cat_surface2);
+
+	cat_rect2.x = 0;
+	cat_rect2.y = 0;
+	cat_rect2.w = cat_surface2->w;
+	cat_rect2.h = cat_surface2->h;
+
+	SDL_FreeSurface(cat_surface2);
+
+	/*
 	//setting button
 	SDL_Surface* setting_surface = IMG_Load("../../res/testRes/testMainSettingBT.png");
 	setting_bt = SDL_CreateTextureFromSurface(g_renderer, setting_surface);
@@ -89,15 +101,41 @@ gamePlay::gamePlay() {
 		TTF_CloseFont(font);
 
 	}
-
+	*/
+	{ //time bar
+		timebar_rect.x = 0;
+		timebar_rect.y = 290;
+		timebar_rect.w = 0;
+		timebar_rect.h = g_window_margin;
+	}
 
 	{ //score text
 		ifstream file("../../res/testRes/scoreboard.txt"); //read scoreboard
 		getline(file, score);
+		score_int = stoi(score);
+
+		string front_score;
+		string new_score;
+
+
+		//점수 네자리수로 고정(나중에 구현)
+		if (score_int == 0) {
+			front_score = "000";
+			new_score = front_score + score;
+		}
+		else if (score_int > 0 && score_int < 100) {
+			front_score = "00";
+			new_score = front_score + score;
+		}
+		else if (score_int >= 100 && score_int < 1000) {
+			front_score = "0";
+			new_score = front_score + score;
+		}
+		else { new_score = score; }
 
 		TTF_Font* font = TTF_OpenFont("../../res/testRes/Galmuri14.ttf", 30);
 		SDL_Color white = { 255,255,255,0 };
-		SDL_Surface* tmp_surface = TTF_RenderUTF8_Blended(font, score.c_str(), white);
+		SDL_Surface* tmp_surface = TTF_RenderUTF8_Blended(font, new_score.c_str(), white);
 		//std::to_string(score).c_str()
 		score_rect.x = 0;
 		score_rect.y = 0;
@@ -123,14 +161,14 @@ gamePlay::gamePlay() {
 
 gamePlay::~gamePlay() {
 	Mix_FreeMusic(play_music);
-	SDL_DestroyTexture(exp_text);
+	//SDL_DestroyTexture(exp_text);
 	SDL_DestroyTexture(score_text);
 	SDL_DestroyTexture(play_bg);
-	SDL_DestroyTexture(cat);
-	SDL_DestroyTexture(setting_bt);
-	SDL_DestroyTexture(timebar_bg);
-	SDL_DestroyTexture(stack_img);
-	SDL_DestroyTexture(playground_img);
+	//SDL_DestroyTexture(cat);
+	//SDL_DestroyTexture(setting_bt);
+	//SDL_DestroyTexture(timebar_bg);
+	//SDL_DestroyTexture(stack_img);
+	//SDL_DestroyTexture(playground_img);
 	SDL_Quit();
 	TTF_Quit();
 }
@@ -146,7 +184,7 @@ void gamePlay::HandleEvents() {
 
 		case SDL_KEYDOWN:
 			if (event.key.keysym.sym == SDLK_SPACE) {
-				changePhaseToEnding();
+				//changePhaseToEnding();
 			}
 			else {
 				Mix_PlayMusic(play_music, -1);
@@ -160,18 +198,20 @@ void gamePlay::HandleEvents() {
 
 void gamePlay::Update() {
 	
-	timebar_rect.w = timebar_bg_dir_rect.w / 4;
+	timebar_rect.w = 50;
 }
 
 void gamePlay::Render() {
 	SDL_RenderCopy(g_renderer, play_bg, NULL, NULL);
 
 
-	
+	//time bar
+	SDL_SetRenderDrawColor(g_renderer, 255, 0, 0, 255); //red
+	SDL_RenderFillRect(g_renderer, &timebar_rect);
 
 
 
-
+	/*
 
 
 	{ //setting button
@@ -199,7 +239,29 @@ void gamePlay::Render() {
 		timebar_bg_dir_rect.h = timebar_bg_rect.h;
 		SDL_RenderCopy(g_renderer, timebar_bg, &timebar_bg_rect, &timebar_bg_dir_rect);
 	}
+	*/
 
+	if (cat_status == false) {
+		//cat image 1
+		SDL_Rect tmp_r;
+		tmp_r.x = 135;
+		tmp_r.y = -110;
+		tmp_r.w = cat_rect.w * 0.45;
+		tmp_r.h = cat_rect.h * 0.45;
+		SDL_RenderCopy(g_renderer, cat, &cat_rect, &tmp_r);
+
+	}
+	else {
+		//cat image 2
+		SDL_Rect tmp_r1;
+		tmp_r1.x = 135;
+		tmp_r1.y = -110;
+		tmp_r1.w = cat_rect2.w * 0.45;
+		tmp_r1.h = cat_rect2.h * 0.45;
+		SDL_RenderCopy(g_renderer, cat2, &cat_rect2, &tmp_r1);
+	}
+
+	/*
 	{ //timebar
 		timebar_rect.x = timebar_bg_dir_rect.x;
 		timebar_rect.y = timebar_bg_dir_rect.y;
@@ -210,8 +272,8 @@ void gamePlay::Render() {
 		SDL_RenderFillRect(g_renderer, &timebar_rect);
 
 
-	}
-
+	}*/
+	/*
 	{//stack img
 		SDL_Rect tmp_r;
 		tmp_r.x = g_window_margin;
@@ -230,7 +292,7 @@ void gamePlay::Render() {
 		SDL_RenderCopy(g_renderer, playground_img, &playground_rect, &tmp_r);
 	}
 
-
+	
 	{ //EXP text
 		SDL_Rect tmp_r;
 		tmp_r.x = g_window_margin;
@@ -239,13 +301,13 @@ void gamePlay::Render() {
 		tmp_r.h = exp_rect.h;
 		SDL_RenderCopy(g_renderer, exp_text, &exp_rect, &tmp_r);
 	}
-
+	*/
 	{ //score text
 		SDL_Rect tmp_r;
-		tmp_r.x = g_window_margin + exp_rect.w;
-		tmp_r.y = g_window_margin * 2;
-		tmp_r.w = score_rect.w;
-		tmp_r.h = score_rect.h;
+		tmp_r.x = g_window_margin + 80;
+		tmp_r.y = 37;
+		tmp_r.w = score_rect.w * 0.8;
+		tmp_r.h = score_rect.h * 0.8;
 		SDL_RenderCopy(g_renderer, score_text, &score_rect, &tmp_r);
 	}
 
@@ -253,8 +315,8 @@ void gamePlay::Render() {
 }
 
 void gamePlay::changePhaseToEnding() {
-	g_current_game_phase = PHASE_ENDING_CLEAR;
+	/*g_current_game_phase = PHASE_ENDING_CLEAR;
 	Mix_HaltMusic();
-	Mix_PlayMusic(clear_music, -1);
+	Mix_PlayMusic(clear_music, -1);*/
 
 }
