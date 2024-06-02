1cbdef22ccf18d0f04bfa33ab736f677af3769c9
Cybecho_Proxy <42949995+Cybecho@users.noreply.github.com>
2024-06-02 22:15:19 +0900

    백업파일 추가

---
+++ src_backup_0602_2215/gameEndingGameover.cpp
@@ -0,0 +1,175 @@
+#include "gameEndingGameover.h"
+
+bool gameover_reset = false;
+
+gameEndingGameover::gameEndingGameover() {
+	bg.resize(GAMEOVER_BG_IMG);
+	cat.resize(GAMEOVER_CAT_IMG);
+	mainBT_rect = { 73,424,173,55 };
+	retryBT_rect = { 293,424,173,55 };
+	cat_cut = 0;
+	bt_clickable = false;
+
+	// 게임오버 엔딩 이미지 로드
+	bg[0].SetImgClassSurface(IMG_Load("../../res/ending/ending_over/ending_over_cut1.png"));
+	bg[1].SetImgClassSurface(IMG_Load("../../res/ending/ending_over/ending_over_cut2.png"));
+	cat[0].SetImgClassSurface(IMG_Load("../../res/ending/ending_over/ending_over_cat_1.png"));
+	cat[1].SetImgClassSurface(IMG_Load("../../res/ending/ending_over/ending_over_cat_2.png"));
+	cat[2].SetImgClassSurface(IMG_Load("../../res/ending/ending_over/ending_over_cat_3.png"));
+	cat[3].SetImgClassSurface(IMG_Load("../../res/ending/ending_over/ending_over_cat_4.png"));
+	cat[4].SetImgClassSurface(IMG_Load("../../res/ending/ending_over/ending_over_cat_5.png"));
+	a.SetImgClassSurface(IMG_Load("../../res/ending/1.jpg"));
+	b.SetImgClassSurface(IMG_Load("../../res/ending/2.jpg"));
+
+	// 텍스처 생성
+	for (auto& i : bg) {
+		i.SetImgClassTexture(SDL_CreateTextureFromSurface(g_renderer, i.GetImgClass().surface));
+		i.SetImgClassSrcRect({ 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT });
+		i.SetImgClassDstRect({ 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT });
+		SDL_FreeSurface(i.GetImgClass().surface);
+	}
+	for (auto& i : cat) {
+		i.SetImgClassTexture(SDL_CreateTextureFromSurface(g_renderer, i.GetImgClass().surface));
+		i.SetImgClassSrcRect({ 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT });
+		i.SetImgClassDstRect({ 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT });
+		SDL_FreeSurface(i.GetImgClass().surface);
+	}
+
+	// BGM and SoundEffect
+	gameover_music = Mix_LoadMUS("../../res/testRes/testBGM2.mp3");
+	if (gameover_music == 0) {
+		printf("Couldn't load the wav: %s\n", Mix_GetError());
+	}
+	SoundEffect = Mix_LoadWAV("../../res/testRes/testSound.mp3");
+}
+
+gameEndingGameover::~gameEndingGameover() {
+	for (auto& i : bg) {
+		SDL_DestroyTexture(i.GetImgClass().texture);
+	}
+	for (auto& i : cat) {
+		SDL_DestroyTexture(i.GetImgClass().texture);
+	}
+	Mix_FreeMusic(gameover_music);
+	Mix_FreeChunk(SoundEffect);
+}
+
+void gameEndingGameover::HandleEvents() {
+	SDL_Event event;
+	if (SDL_PollEvent(&event)) {
+		switch (event.type) {
+		case SDL_QUIT:
+			g_flag_running = false;
+			break;
+
+		case SDL_KEYDOWN:
+			// Spacebar (cheat key)
+			if (event.key.keysym.sym == SDLK_SPACE) {
+				cout << "Cheat key has been used!" << endl;
+				g_current_game_phase = PHASE_INTRO;
+				intro_reset = true;
+				Mix_HaltMusic();
+				Mix_PlayMusic(intro_music, -1);
+			}
+			break;
+
+		default:
+			break;
+		}
+
+		// 버튼 동작 부분: 아래의 코드 활성
+		if (event.type == SDL_MOUSEBUTTONDOWN && bt_clickable) {
+			if (event.button.button == SDL_BUTTON_LEFT) {
+				int mouseX = event.button.x;
+				int mouseY = event.button.y;
+
+				//click main button
+				if (mouseX > mainBT_rect.x && mouseY > mainBT_rect.y &&
+					mouseX < mainBT_rect.x + mainBT_rect.w && mouseY < mainBT_rect.y + mainBT_rect.h) {
+					g_current_game_phase = PHASE_MAIN;
+					gameover_reset = true; // 추가
+					bt_clickable = false;
+					Mix_PlayChannel(-1, SoundEffect, 0);
+					Mix_HaltMusic();
+					Mix_PlayMusic(main_music, -1);
+				}
+
+				//click retry button
+				if (mouseX > retryBT_rect.x && mouseY > retryBT_rect.y &&
+					mouseX < retryBT_rect.x + retryBT_rect.w && mouseY < retryBT_rect.y + retryBT_rect.h) {
+					g_current_game_phase = PHASE_PLAYING;
+					gameover_reset = true; // 추가
+					bt_clickable = false;
+					Mix_PlayChannel(-1, SoundEffect, 0);
+					Mix_HaltMusic();
+					Mix_PlayMusic(play_music, -1);
+				}
+			}
+		}
+	}
+}
+
+void gameEndingGameover::Update() {
+	if (gameover_reset) {
+		for (int i = 0; i < GAMEOVER_BG_IMG-1; i++) {
+			bg[i].InitFade();
+			bg[i].InitCrossFade();
+			bg[i].InitDelay();
+		}
+		for (int i = 0; i < GAMEOVER_CAT_IMG - 1; i++) {
+			cat[i].InitFade();
+			cat[i].InitCrossFade();
+			cat[i].InitDelay();
+		}
+		cat_cut = 0;
+		bt_clickable = false;
+		gameover_reset = false;
+	}
+
+	isChanged = false;
+	timebar_rect.w = 540;
+	sec = 0;
+}
+
+void gameEndingGameover::Render() {
+	SDL_SetRenderDrawColor(g_renderer, 0, 0, 0, 255);
+	SDL_RenderClear(g_renderer); // clear the renderer to the draw color
+
+	if (!gameover_reset) { // 1틱동안 이전 엔딩 화면이 뜨는 오류 방지
+
+		// 배경 등장 부분
+		bg[0].FadeIn(1);
+		if (bg[0].isFadeFinished()) {
+			bg[0].Delay(1.5);
+
+			// 배경 전환 부분
+			if (bg[0].isDelayFinished()) {
+				bg[0].CrossFade(bg[1].GetImgClass(), 0.5); // 0.5초간 다음 배경 이미지 CrossFade
+			}
+
+			if (bg[0].isCrossFadeFinished()) {
+				bt_clickable = true;
+
+				// GAMEOVER_BG_IMG 애니메이션이 완료된 후에 GAMEOVER_CAT_IMG 애니메이션 시작
+				if (cat_cut == GAMEOVER_CAT_IMG - 1)
+					cat[cat_cut].CrossFade(cat[0].GetImgClass(), 0.2);
+				else
+					cat[cat_cut].CrossFade(cat[cat_cut + 1].GetImgClass(), 0.2);
+
+				if (cat[cat_cut].isCrossFadeFinished())
+					cat[cat_cut].Delay(0.7);
+
+				if (cat[cat_cut].isDelayFinished()) {
+					cat[cat_cut].InitCrossFade();
+					cat[cat_cut].InitDelay();
+
+					cat_cut++;
+					if (cat_cut == GAMEOVER_CAT_IMG)
+						cat_cut = 0;
+				}
+			}
+		}
+	}
+
+	SDL_RenderPresent(g_renderer); // draw to the screen
+}
---
+++ src_backup_0602_2215/gamePlay.h
@@ -0,0 +1,147 @@
+#pragma once
+#include "gameClass.h"
+#include "gameLogic.h"
+//play scene
+
+
+class gamePlay : public gameClass {
+public:
+	
+	//! ******************** 생성자 소멸자 ******************** 
+	gamePlay();
+	virtual ~gamePlay();
+	
+	//! ********************** 기본 함수 **********************
+	void HandleEvents();
+	void Update();
+	void Render();
+	void resetGame();
+
+	//! ********************** 페이즈 전환 **********************
+	void changePhase(GamePhase status);
+	void gotoHome();
+
+	//! ********************** 타이머 관련 **********************
+	void resetTimer();
+	void stageLimitTime();
+	void updateTimer();
+	void changeTimebar();
+	void checkQuit();
+	
+	//! ********************** 점수 관련 **********************
+	void updateScore(int s);
+	void addSeconds(int seconds);
+	void updateAddScore();
+	void writeScore(const string& s);
+
+	//! ********************** 데이터 import 및 Rendering **********************
+	void loadIMGs();
+	void loadSounds();
+	void loadTxts();
+	void renderCat();
+	void renderSetting();
+
+	//! ********************** 마우스 이벤트 **********************
+	void MouseButtonEvents();
+	
+
+	//! ************************** gameLogic **************************
+	void increaseLevelLogic();			//~ 레벨 증가 로직
+	void loadMahjongBlocks();			//~ 맞춰야 할 블록 로드
+	void checkAndLoadMahjongBlocks();	//~ 맞춰야 할 블록 체크 및 로드
+	void checkGameStatus();				//~ 게임 상태 체크 (g_status 상태 확인)
+
+	//! ************************** 고양이 상태 **************************
+	void changeCatAnimation();
+	void waterAnimation();
+	void sleepAnimation();
+	void fishAnimation();
+	void mouseAnimation();
+
+
+	//! ************************** getter & setter **************************
+	int getLastSec() const { return last_sec; }
+	void setLastSec(int sec) { last_sec = sec; }
+
+	
+
+protected:
+	SDL_Event event;
+	gameLogic m_gameLogic; //~ 게임 로직 클래스 생성
+
+	//const Uint32 p_start_time_ms;
+	Uint32 p_last_time_ms;
+	string string_time;
+	string update_score; //누적 점수
+	//int update_score; //new score
+	//float timebarw;
+
+	SDL_Texture* play_bg;
+	SDL_Texture* playground_img;
+	SDL_Texture* stack_img;
+	SDL_Texture* timebar_bg;
+	SDL_Texture* cat;
+	SDL_Texture* cat2;
+	SDL_Texture* setting_bt;
+	SDL_Texture* setting;
+	SDL_Texture* score_text2; //get score from txt file
+
+	TTF_Font* score_font;
+	
+	SDL_Texture* cat_sleep;
+	SDL_Texture* cat_walk_left;
+	SDL_Texture* cat_walk_right;
+	SDL_Texture* cat_mouse;
+	SDL_Texture* cat_fish;
+	SDL_Texture* cat_scratcher;
+	SDL_Texture* cat_water;
+
+	SDL_Rect playground_rect;
+	SDL_Rect stack_rect;
+	
+	SDL_Rect cat_rect;
+	SDL_Rect cat_rect2;
+	SDL_Rect cat_play_rect; //스프라이트 그림 4개일때
+	SDL_Rect cat_play_rect2; //스프라이트 그림 3개일때
+	SDL_Rect settingBT_rect;
+
+	//score
+	SDL_Rect exp_rect;
+	SDL_Rect score_rect;
+
+	Mix_Chunk* setting_SoundEffect;
+
+	int stage;
+	int count_;
+	int Cat_Ani_Status; //cat animation status // 0 : 목마름, 1 : 배부름,  2 : 놀아주기,  3 : 잠자기
+	
+	int limit_sec;
+	int last_sec; //last second (stage second)
+	Uint32 lastMatchTime; // 추가 점수 확인용
+	int addScore;	// 추가 점수 멤버 변수
+};
+
+extern Mix_Music* main_music;
+extern Mix_Music* play_music;
+extern Mix_Music* clear_music;
+extern Mix_Music* gameover_music;
+extern bool cat_status;
+extern bool clear_reset;
+extern bool gameover_reset;
+
+extern Uint32 g_last_time_ms;
+extern bool isChanged;
+extern bool isForcedQuit;
+extern bool isBasicCat; //고양이 기본 상태
+
+extern SDL_Rect timebar_rect;
+
+///  점수 관련
+extern string score;
+extern string original_score;
+extern int org_score_int; // 게임 시작 전 메모장에 저장된 점수
+extern int total_Score_int; // 누적 총 점수(메인화면)
+
+/// 
+extern int sprite_num; //고양이 스프라이트 x좌표
+extern int sec;
